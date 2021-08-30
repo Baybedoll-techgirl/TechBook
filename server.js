fc4fe3f118c6137f658f8361dd549319593a392f
Babydoll-techgirl <ritajack@gmail.com>
2021-08-29 21:59:41 -0400

    final commit

--- server.js
+++ server.js
@@ -1,61 +1,81 @@
-const express = require('express');
-const app = express();
-const { pool } = require('./dbConfig');
-const bcrypt = require('bcrypt');
-const session = require('express-session');
-const flash = require('express-flash');
-const passport = require('passport');
+  const path = require('path');
+ 
+  //express server
+  const express = require('express');
+  const app = express();
+  const http = require('http');
+  const server = http.createServer(app);
+  
+  //socket.io
+  const { Server } = require("socket.io");
+  const io = new Server(server);
+  const socketio = require('socket.io');
+  const formatMessage = require('./utils/messages');
+  const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+  } = require('./utils/users');
 
-
-const initializePassport = require('./passportConfig');
-const initialize = require('./passportConfig');
-const { read } = require('fs');
-const { networkInterfaces } = require('os');
-
-initializePassport(passport);
-
-const PORT = process.env.PORT || 5000;
-
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:false}));
-
-app.use(
-    session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: false
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(flash());
-   
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-app.get('/users/register', checkAuthenticated, (req, res) => {
-    res.render("register");
-});
-app.get('/users/login',checkAuthenticated, (req, res) => {
-    res.render("login");
-});
-app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
-    // res.sendFile(__dirname + '/public/index.html');
-    res.render("dashboard", {user: req.user.name});
-    
-});
-app.get('/chat', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+//database
+const { pool } = require('./dbConfig');  
+const User = require("./utils/users");
+  
+  //authentication
+  const bcrypt = require('bcrypt'); 
+  const initializePassport = require('./passportConfig');
+  const passport = require('passport');
+  const flash = require('express-flash'); 
+  const session = require('express-session'); 
+  const methodOverride = require('method-override');
+  initializePassport(
+    passport,
+    email => User.findOne({email: email}),
+    id => User.findOne({id: id})
+    )
+  
+  
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('view-engine', 'ejs');
+  app.use(express.urlencoded({ extended: false })); 
+  app.use(flash())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, 
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session()) 
+  app.use(methodOverride('_method'))
+  
+  //Routes
+  app.get('/', checkAuthenticated, (req, res) => {
+    res.sendFile(__dirname + 'index.html')
   });
   
-app.get('/users/logout', (req,res)=> {
+  app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+  });
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate ('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
+    
+  app.get('/register', checkNotAuthenticated, async (req, res) => {
+    res.render('register.ejs');
+  });
+   
+  app.get('/logout', (req, res) => {
     req.logOut();
     req.flash("success_msg", "You have logged out");
-    res.redirect('/users/login');
-})
-app.post('/users/register', async (req, res) => {
+    res.redirect('/login');
+    
+  });
+  app.post('/register', async (req, res) => {
     let {name, email, password, password2} = req.body;
     console.log({
         name,
@@ -64,7 +84,7 @@ app.post('/users/register', async (req, res) => {
         password2
     }); 
 
-    let errors = [];
+  let errors = [];
 
     if(!name || !email || !password || !password2) {
         errors.push({message: "Please enter all fields"});
@@ -95,7 +115,7 @@ app.post('/users/register', async (req, res) => {
 
                 if(results.rows.length > 0) {
                     errors.push({message: "Email already registered"});
-                    res.render('register', {errors});
+                    res.render('register.ejs', {errors});
                 }else{
                     pool.query(
                         `INSERT INTO users (name, email, password)
@@ -107,88 +127,88 @@ app.post('/users/register', async (req, res) => {
                             }
                             console.log(results.rows);
                             req.flash('success_msg', "You are now registered. Please log in");
-                            res.redirect('/users/login');
+                            res.redirect('/login');
                         }
                     )
+                      
                 }
             } 
         );
-    }
-});
+    };
+  });
 
-app.post('/users/login', passport.authenticate('local', {
-    successRedirect: '/users/dashboard',
-    failureRedirect: '/users/login',
-    failureFlash: true
 
-}));
-function checkAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return res.redirect('/users/dashboard');
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
     }
-    next();
-}
-function checkNotAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
+    res.redirect('/login')
+  }
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
     }
-   res.redirect('/users/login'); 
-}
-
-const botName = 'TechChat Bot';
+    next()
+  }
+  
+  const botName = 'TechChat Bot';
 
 // Run when user connects
 io.on('connection', socket => {
-    socket.on('joinRoom', ({ username, room }) => {
-      const user = userJoin(socket.id, username, room);
-  
-      socket.join(user.room);
-  
-      // Welcome current user
-      socket.emit('message', formatMessage(botName, 'Welcome to TechChat!'));
-  
-      // Broadcast when a user connects
-      socket.broadcast
-        .to(user.room)
-        .emit(
-          'message',
-          formatMessage(botName, `${user.username} has joined the chat`)
-        );
-  
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to TechChat!'));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when user disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
       // Send users and room info
       io.to(user.room).emit('roomUsers', {
         room: user.room,
         users: getRoomUsers(user.room)
       });
-    });
-  
-    // Listen for chatMessage
-    socket.on('chatMessage', msg => {
-      const user = getCurrentUser(socket.id);
-  
-      io.to(user.room).emit('message', formatMessage(user.username, msg));
-    });
-  
-    // Runs when user disconnects
-    socket.on('disconnect', () => {
-      const user = userLeave(socket.id);
-  
-      if (user) {
-        io.to(user.room).emit(
-          'message',
-          formatMessage(botName, `${user.username} has left the chat`)
-        );
-  
-        // Send users and room info
-        io.to(user.room).emit('roomUsers', {
-          room: user.room,
-          users: getRoomUsers(user.room)
-        });
-      }
-    });
+    }
   });
-  
-
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}.`)
 });
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 3000;
+  }
+  
+  server.listen(port, () => {
+    console.log('Server has started successfully');
+  });
